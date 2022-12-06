7a2a65bd33b4263ac7c9a00b5993ad6572a34145
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2022-12-06 23:38:11 +0700

    Update alive.py

--- YinsRobot/modules/alive.py
+++ YinsRobot/modules/alive.py
@@ -9,17 +9,17 @@ from YinsRobot.events import register
 from YinsRobot import telethn as tbot
 
 yinzver = "2.0.22"
-PHOTO = "https://telegra.ph/file/64f4b1e3ab6d6911447d2.jpg"
+PHOTO = "https://telegra.ph/file/7ef1d87d0b20ee66d6f8f.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Yins Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Gugun Robot.** \n\n"
   TEXT += "✨ **I'm Working Properly** \n\n"
-  TEXT += f"✨ **Oᴡɴᴇʀ : [AʏɪɪɴXᴅ](https://t.me/AyiinXd)** \n\n"
+  TEXT += f"✨ **Oᴡɴᴇʀ : [ᴍʏ ʙᴏss](https://t.me/PANGGIL_AJA_MAS)** \n\n"
   TEXT += f"✨ **Lɪʙʀᴀʀʏ Vᴇʀsɪᴏɴ    :** `{telever}` \n\n"
   TEXT += f"✨ **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ   :** `{tlhver}` \n\n"
   TEXT += f"✨ **Pʏʀᴏɢʀᴀᴍ Vᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
-  TEXT += f"✨ **YɪɴᴢRᴏʙᴏᴛ Vᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
+  TEXT += f"✨ **GugunRᴏʙᴏᴛ Vᴇʀsɪᴏɴ :** `{yinzver}` \n\n"
   TEXT += "**Thanks For Adding Me Here ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/YinzRobot?start=help"), Button.url("Support", "https://t.me/AyiinXdSupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Assistenmusik_bot?start=help"), Button.url("Support", "https://t.me/+dgaE6Hme_9I4ZGNl")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
