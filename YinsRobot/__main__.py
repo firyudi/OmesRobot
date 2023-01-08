6f138fa609933d84af038d9006a5cccef970b541
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2023-01-08 17:53:46 +0700

    Update __main__.py

--- YinsRobot/__main__.py
+++ YinsRobot/__main__.py
@@ -368,7 +368,7 @@ def yins_about_callback(update, context):
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
             "\n\n_Yin's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for YinsRobot.",
+            "\n\n Click on button bellow to get basic help for TRYMASKOS.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
